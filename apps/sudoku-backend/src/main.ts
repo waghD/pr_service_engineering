36d4953e078f8b431d05ce33121e05b42f53a45d
waghD <d.waghubinger@gmx.at>
2021-10-15 11:01:52 +0200

    change backend api port to 8080

--- apps/sudoku-backend/src/main.ts
+++ apps/sudoku-backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 8003;
+  const port = process.env.PORT || 8080;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
