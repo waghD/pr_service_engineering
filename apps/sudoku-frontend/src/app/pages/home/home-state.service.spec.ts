baf7e5214cfdc31f3190e1eaaa3ccc1784fca6e1
waghD <d.waghubinger@gmx.at>
2021-10-15 11:29:05 +0200

    fix frontend unit tests

--- apps/sudoku-frontend/src/app/pages/home/home-state.service.spec.ts
+++ apps/sudoku-frontend/src/app/pages/home/home-state.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HomeStateService } from './home-state.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('HomeStateService', () => {
   let service: HomeStateService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(HomeStateService);
   });
 
--- apps/sudoku-frontend/src/app/pages/home/home.component.spec.ts
+++ apps/sudoku-frontend/src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HomeStateService } from './home-state.service';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,14 +10,15 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      imports: [HttpClientTestingModule],
+      declarations: [ HomeComponent ],
+      providers: [HomeStateService]
     })
     .compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
