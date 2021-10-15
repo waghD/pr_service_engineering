0df7c2798ddb52cecc3803a7011746c0b23f0c98
waghD <d.waghubinger@gmx.at>
2021-10-15 11:55:46 +0200

    add demo guard

--- apps/sudoku-frontend/src/app/app-routing.module.ts
+++ apps/sudoku-frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DemoGuard } from './guards/demo.guard';
 
 const routes: Routes = [
   {
@@ -8,7 +9,8 @@ const routes: Routes = [
   },{
     path: '',
     redirectTo: '/home',
-    pathMatch: 'full'
+    pathMatch: 'full',
+    canActivate: [DemoGuard]
   },
   {
     path: '**',
