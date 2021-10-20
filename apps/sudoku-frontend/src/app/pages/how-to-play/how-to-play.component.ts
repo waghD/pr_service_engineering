f6dc05649e750c87fe1274a3c96987055bd27a69
Marco Stadler <stadler.marco@outlook.com>
2021-10-20 19:44:13 +0200

    Base setup of the new pages -> how to play page

---
+++ apps/sudoku-frontend/src/app/pages/how-to-play/how-to-play.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'se-sudoku-how-to-play',
+  templateUrl: './how-to-play.component.html',
+  styleUrls: ['./how-to-play.component.scss']
+})
+export class HowToPlayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
