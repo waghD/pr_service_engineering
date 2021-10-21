de4d8c023babe06d0f5a0b3e2645e351a547626a
Marco Stadler <stadler.marco@outlook.com>
2021-10-21 15:03:57 +0200

    Impress component finished

--- apps/sudoku-frontend/src/app/app-routing.module.ts
+++ apps/sudoku-frontend/src/app/app-routing.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DemoGuard } from './guards/demo.guard';
+import { ImpressComponent } from './pages/impress/impress.component';
+import { HowToPlayComponent } from './pages/how-to-play/how-to-play.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
 
 const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
     canActivate: [DemoGuard]
-  },{
+  }, {
     path: '',
     redirectTo: '/home',
     pathMatch: 'full'
+  }, {
+    path: 'impress',
+    component: ImpressComponent
+  },
+  {
+    path: 'how-to-play',
+    component: HowToPlayComponent
+  }, {
+    path: 'privacy',
+    component: PrivacyComponent
   },
   {
     path: '**',
@@ -20,8 +33,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
+    RouterModule.forRoot(routes, { initialNavigation: 'enabled' })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
