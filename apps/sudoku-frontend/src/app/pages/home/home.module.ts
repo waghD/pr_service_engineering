e32ee26c468bfd515dde808ece5b5a655b08b305
Marco Stadler <stadler.marco@outlook.com>
2021-10-22 11:58:40 +0200

    move footer and header to shared components

--- apps/sudoku-frontend/src/app/pages/home/home.module.ts
+++ apps/sudoku-frontend/src/app/pages/home/home.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
-import { HeaderComponent } from '../header/header.component';
+import { HeaderComponent } from '../../shared/components/header/header.component';
 import { ImpressComponent } from '../impress/impress.component';
 import { HowToPlayComponent } from '../how-to-play/how-to-play.component';
 import { PrivacyComponent } from '../privacy/privacy.component';
 import { AppModule } from '../../app.module';
-import { FooterComponent } from '../footer/footer.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 
 
 const routes: Routes = [
