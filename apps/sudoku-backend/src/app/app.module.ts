b7afcf5f5312f8e84fda5b8735babddb634da671
vambi <simon.vamberszky@live.de>
2021-10-23 16:12:35 +0200

    set up Database

--- apps/sudoku-backend/src/app/app.module.ts
+++ apps/sudoku-backend/src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
-
+import {ConfigModule} from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './services/app.service';
 import { TestController } from './controllers/test/test.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot( {isGlobal:true}),
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      database: "sudoku",
+      autoLoadEntities:true,
+      synchronize:true
+
+    })
+  ],
   controllers: [AppController, TestController],
   providers: [AppService],
 })
