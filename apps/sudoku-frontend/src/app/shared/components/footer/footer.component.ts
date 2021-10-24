c21970333863379d1430feb1360f64daeb03bbf1
Marco Stadler <stadler.marco@outlook.com>
2021-10-24 14:40:06 +0200

    fixed footer on home

--- apps/sudoku-frontend/src/app/shared/components/footer/footer.component.ts
+++ apps/sudoku-frontend/src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'se-sudoku-footer',
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  @Input() additionalClass: string;
+
+  constructor() {
+    this.additionalClass = '';
+  }
 
 }
