41365e6dceb2f3b95802bc804ce5ec8f946e9d64
waghD <d.waghubinger@gmx.at>
2021-10-24 20:52:00 +0200

    add state and logic to login no email or password input required for redirect

--- apps/sudoku-frontend/src/app/pages/login/login.component.ts
+++ apps/sudoku-frontend/src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthStateService } from '../../services/auth-state.service';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'se-sudoku-login',
@@ -9,7 +10,13 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
+  loginForm: FormGroup;
+
   constructor(private router: Router, private authService: AuthStateService) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [Validators.required])
+    });
   }
 
   login() {
