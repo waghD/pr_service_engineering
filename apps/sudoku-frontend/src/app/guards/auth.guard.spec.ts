7dc2907aad64516d4b3939846a271a62f0823a2d
waghD <d.waghubinger@gmx.at>
2021-10-25 10:33:06 +0200

    implement unit tests

--- apps/sudoku-frontend/src/app/guards/auth.guard.spec.ts
+++ apps/sudoku-frontend/src/app/guards/auth.guard.spec.ts
@@ -1,16 +1,82 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthGuard } from './auth-guard.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthStateService } from '../services/auth-state.service';
+import { Component } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+const loginRouteStateMock = {
+  url: [{
+    path: 'login'
+  }]
+} as ActivatedRouteSnapshot;
+
+const homeRouteStateMock = {
+  url: [{
+    path: 'home'
+  }]
+} as ActivatedRouteSnapshot;
+
+@Component({
+  template: '<router-outlet></router-outlet>'
+})
+class RoutingComponent { }
+
+@Component({
+  template: ''
+})
+class DummyComponent { }
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
+  let authService: AuthStateService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes([
+          {path: 'login', component: DummyComponent},
+          {path: 'home', component: DummyComponent},
+          {path: '', pathMatch: 'full', redirectTo: '/home'}
+        ])
+      ],
+      declarations: [DummyComponent, RoutingComponent],
+      providers: [AuthStateService]
+    });
     guard = TestBed.inject(AuthGuard);
+    authService = TestBed.inject(AuthStateService);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
   });
+
+  it('should block access for unauthenticated', () => {
+    const canActivate = guard.canActivate(homeRouteStateMock, {url: '/home'} as RouterStateSnapshot);
+    expect(canActivate).toBe(false);
+  })
+
+  it('should allow access for authenticated', () => {
+    authService.login();
+    const canActivate = guard.canActivate(homeRouteStateMock, {url: '/home'} as RouterStateSnapshot);
+    expect(canActivate).toBe(true);
+  })
+
+  it('should allow access to /login for unauthenticated', () => {
+    const canActivate = guard.canActivate(loginRouteStateMock, {url: '/login'} as RouterStateSnapshot);
+    expect(canActivate).toBe(true);
+  })
+
+  it('should block access to /login for authenticated', () => {
+    authService.login();
+    const canActivate = guard.canActivate(loginRouteStateMock, {url: '/login'} as RouterStateSnapshot);
+    expect(canActivate).toBe(false);
+  })
+
+  it('should store blocked url in redirect', () => {
+    guard.canActivate(homeRouteStateMock, {url: '/home'} as RouterStateSnapshot);
+    expect(authService.hasRedirectPage()).toBe(true);
+    expect(authService.consumeRedirectPage()).toEqual('/home');
+  })
 });
--- apps/sudoku-frontend/src/app/pages/login/login.component.spec.ts
+++ apps/sudoku-frontend/src/app/pages/login/login.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthStateService } from '../../services/auth-state.service';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,6 +13,11 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, CommonModule],
+      providers: [{
+        provide: Router,
+        useClass: RouterTestingModule
+      }, AuthStateService],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
