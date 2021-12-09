32d1537b2c2ae4926097f3496ec03369013b8f12
Marco Stadler <51763897+rrancerr@users.noreply.github.com>
2021-12-09 13:32:11 +0100

    Classic game finalization (#48)
    * Added functionality for checking 3x3 boxes
    
    * Added additional helper-highlighting
    
    + refactored highlight methods
    + added highlighting for current selection
    
    * Patch: Bug Fix: Made initial fields non-editable
    
    * Added time control
    
    * Added conceal/reveal grid functionality
    
    * intermediate commit right side control
    
    * save sudoku button and functionality
    
    * Toggle Switch button for helper mode added
    
    * Back to menu button and save time addition
    
    * added right control styling
    
    * scaled sudoku to maximum
    
    * add required modules to unit tests
    
    * passed suoku entity in generics
    
    * merging scss

--- apps/sudoku-frontend/src/app/app.module.ts
+++ apps/sudoku-frontend/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- apps/sudoku-frontend/src/app/pages/classic-game/classic-game.service.ts
+++ apps/sudoku-frontend/src/app/pages/classic-game/classic-game.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { SudokuEntity } from '../../../../../../libs/models/sudoku.entity';
+import { SudokuFieldEntity } from '../../../../../../libs/models/sudoku-field.entity';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,18 @@ export class ClassicGameService {
   getNewRandomSudoku(): Observable<SudokuEntity> {
     return this.http
       .post<SudokuEntity>(this.baseApiURL + '/sudokus/generate', {});
+  }
+
+  //TODO: if login is implemented, pass additionally the user id
+  saveSudokuFields(sudokuId: number, gridData: { x: number, y: number, value: number }[]): Observable<SudokuFieldEntity[]> {
+    console.log(`SudokuID=${sudokuId}`);
+    return this.http
+      .post<SudokuFieldEntity[]>(this.baseApiURL + '/sudokus/' + sudokuId + '/fields', gridData);
+  }
+
+  //TODO: if login is implemented, pass additionally the user id
+  saveSudokuParams(sudokuId: number, secondsOfTimer: { edit_time: number }) {
+    return this.http
+      .put<SudokuEntity>(this.baseApiURL + '/sudokus/' + sudokuId, secondsOfTimer);
   }
 }
