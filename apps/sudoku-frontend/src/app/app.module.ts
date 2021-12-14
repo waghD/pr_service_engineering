6af73de035a29fcd6cb673727cd6ef5f9a63deb0
Marco Stadler <stadler.marco@outlook.com>
2021-12-14 15:12:33 +0100

    Created blank component

--- apps/sudoku-frontend/src/app/app.module.ts
+++ apps/sudoku-frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthInterceptor } from './services/auth.interceptor';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,7 +14,8 @@ import { AuthInterceptor } from './services/auth.interceptor';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
---
+++ apps/sudoku-frontend/src/app/shared/components/login-banner/login-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'se-sudoku-login-banner',
+  templateUrl: './login-banner.component.html',
+  styleUrls: ['./login-banner.component.scss']
+})
+export class LoginBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
