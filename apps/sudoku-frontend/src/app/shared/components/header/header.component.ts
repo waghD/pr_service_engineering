8d8513678b8d486d1e02764669c53d45e3261d35
Marco Stadler <stadler.marco@outlook.com>
2022-01-25 15:19:22 +0100

    fixe url prefix

--- apps/sudoku-frontend/src/app/shared/components/header/header.component.ts
+++ apps/sudoku-frontend/src/app/shared/components/header/header.component.ts
@@ -12,10 +12,12 @@ export class HeaderComponent {
   @Input() difficulty?: string;
 
   GAME_PAGE_URLS: string[];
+  routerUrlPrefix: string;
 
   constructor(public router: Router) {
     this.pageHeading = '';
     this.additionalClass = '';
     this.GAME_PAGE_URLS = ['/diagonal-game', '/classic-game', '/color-game', '/diagonal-color-game', '/region-game'];
+    this.routerUrlPrefix = this.router.url.split('?')[0];
   }
 }
