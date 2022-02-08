59ce69b983f68076f7de12891df1d41083f2cb82
Marco Stadler <stadler.marco@outlook.com>
2022-02-08 23:04:13 +0100

    fix unit tests

--- apps/sudoku-frontend/src/app/pages/home/sudoku-type-selection/sudoku-type-selection.component.spec.ts
+++ apps/sudoku-frontend/src/app/pages/home/sudoku-type-selection/sudoku-type-selection.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SudokuTypeSelectionComponent } from './sudoku-type-selection.component';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SudokuTypeSelectionComponent', () => {
   let component: SudokuTypeSelectionComponent;
@@ -8,9 +11,15 @@ describe('SudokuTypeSelectionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SudokuTypeSelectionComponent ]
+      declarations: [SudokuTypeSelectionComponent],
+      imports: [MatDialogModule, RouterTestingModule],
+      providers: [SudokuTypeSelectionComponent, {
+        provide: MatDialogRef,
+        useValue: {}
+      }],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
